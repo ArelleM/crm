402199fda8627070a6e603c8dbf8a233f0f0ab2a
Arnaudsau <arnaud.saudrais@yahoo.com>
2023-06-06 11:31:12 +0200

    Add register and login

--- script.js
+++ script.js
@@ -31,4 +31,55 @@ function showMainPage() {
         userDetails.innerHTML = `Prénom: ${data.data.first_name} <br> Nom: ${data.data.last_name} <br> Email: ${data.data.email}`;
       });
   }
+
+  // Ajouter les fonctions suivantes à votre fichier script.js
+
+function register() {
+  let email = document.getElementById('registerEmail').value;
+  let password = document.getElementById('registerPassword').value;
+
+  fetch('https://reqres.in/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.id && data.token) {
+      showMainPage();
+    } else {
+      alert('Registration failed');
+    }
+  });
+}
+
+function login() {
+  let email = document.getElementById('loginEmail').value;
+  let password = document.getElementById('loginPassword').value;
+
+  fetch('https://reqres.in/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.token) {
+      showMainPage();
+    } else {
+      alert('Login failed');
+    }
+  });
+}
+
   
