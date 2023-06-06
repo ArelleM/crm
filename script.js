a33ddbc226060b58a937bc16ed4db0d888e1bd28
ArelleM <clerc.angelique@gmail.com>
2023-06-06 15:24:05 +0200

    working front

--- script.js
+++ script.js
@@ -9,39 +9,28 @@ function showMainPage() {
       const userList = document.getElementById("userList");
       userList.innerHTML = "";
       data.data.forEach((user) => {
-        userList.innerHTML += `<div class="max-w-xs">
+        userList.innerHTML += `<div class="max-w-xs m-4">
     <div class="bg-white shadow-xl rounded-lg py-3">
         <div class="photo-wrapper p-2">
             <img class="w-32 h-32 rounded-full mx-auto" src="${user.avatar}" alt="John Doe">
         </div>
         <div class="p-2">
-            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">Joh Doe</h3>
-            <div class="text-center text-gray-400 text-xs font-semibold">
-                <p>Web Developer</p>
-            </div>
+            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">${user.first_name} ${user.last_name} </h3>
             <table class="text-xs my-3">
-                <tbody><tr>
-                    <td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
-                    <td class="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
-                </tr>
-                <tr>
-                    <td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
-                    <td class="px-2 py-2">+977 9955221114</td>
-                </tr>
+                <tbody>
                 <tr>
                     <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
-                    <td class="px-2 py-2">john@exmaple.com</td>
+                    <td class="px-2 py-2">${user.email}</td>
                 </tr>
             </tbody></table>
 
             <div class="text-center my-3">
-                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
+                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#" onclick="showUser(${user.id})">Voir profil</a>
             </div>
 
         </div>
     </div>
 </div>`;
-        //userList.innerHTML += `<div>${user.first_name} ${user.last_name} <button onclick="showUser(${user.id})">Voir plus</button></div>`;
       });
     });
 }
@@ -79,8 +68,28 @@ function showUser(id) {
   fetch(`https://reqres.in/api/users/${id}`)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       const userDetails = document.getElementById("userDetails");
-      userDetails.innerHTML = `Prénom: ${data.data.first_name} <br> Nom: ${data.data.last_name} <br> Email: ${data.data.email}`;
+      userDetails.innerHTML = `<div class="max-w-xs m-4">
+      <div class="bg-white shadow-xl rounded-lg py-3">
+          <div class="photo-wrapper p-2">
+              <img class="w-32 h-32 rounded-full mx-auto" src="${data.data.avatar}" alt="John Doe">
+          </div><div class="p-2">
+      <h3 class="text-center text-xl text-gray-900 font-medium leading-8">${data.data.first_name} ${data.data.last_name} </h3>
+      <table class="text-xs my-3">
+          <tbody>
+          <tr>
+              <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
+              <td class="px-2 py-2">${data.data.email}</td>
+          </tr>
+      </tbody></table>
+
+      <div class="text-center my-3">
+          <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#" onClick="showMainPage()" >Retour</a>
+      </div>
+
+  </div></div>
+  </div>`;
     });
 }
 
